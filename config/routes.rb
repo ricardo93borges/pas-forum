970c119ff48c12f8a5a995cbeb44dae9ccdb3dae
Ricardo Borges <ricardo93borges@gmail.com>
2018-06-10 18:36:49 -0300

    Change style

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :users
   resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/", to: "questions#index"
 end
