365f2673a79416a3c1a6a1d4a4a60e1d5fb4af60
Ricardo Borges <ricardo93borges@gmail.com>
2018-06-30 18:52:24 -0300

    Fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   # GET /questions
   # GET /questions.json
